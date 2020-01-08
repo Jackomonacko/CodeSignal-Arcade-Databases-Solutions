ff85641dbd0713f558362c3e470066ef90413019
Jackomonacko <jacksadowski33@gmail.com>
2020-01-08 13:18:42 -0500

    Update code.sql

--- Would you LIKE the Second Meal?/suspectsInvestigation/code.sql
+++ Would you LIKE the Second Meal?/suspectsInvestigation/code.sql
@@ -1,6 +1,9 @@
-  SELECT id, name, surname
-    FROM Suspect 
-    WHERE height <= 170
-    AND UPPER(name) LIKE 'B%'
-    AND surname LIKE "Gre_n"
-    ORDER BY id;
+/*Please add ; after each select statement*/
+CREATE PROCEDURE suspectsInvestigation()
+BEGIN
+   SELECT id, name, surname
+   FROM Suspect
+   WHERE height <= 170
+   AND name LIKE 'b%'
+   AND surname LIKE '%gre_n%';
+END
