4e7f0a1727e78f1dc24e9bec32ce2bc241fe0418
Jackomonacko <jacksadowski33@gmail.com>
2020-07-06 17:17:00 -0400

    Create code.sql

---
+++ Time for Tricks/combinationLock/code.sql
@@ -0,0 +1,6 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE combinationLock()
+BEGIN
+  SELECT FLOOR(ROUND(EXP(SUM(LOG( LENGTH(characters) ))),1)) AS combinations
+    FROM discs;
+END
