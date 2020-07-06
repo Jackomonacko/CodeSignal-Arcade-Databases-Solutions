62e823ffce23fb640ebe6b413e7db2fb4bb2eda8
Jackomonacko <jacksadowski33@gmail.com>
2020-07-06 17:21:07 -0400

    Create code.sql

---
+++ Specialties/booksCatalogs/code.sql
@@ -0,0 +1,8 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE booksCatalogs()
+BEGIN
+	SELECT ExtractValue(xml_doc, '/descendant-or-self::author[1]') AS author
+    FROM catalogs
+    GROUP BY author
+    ORDER BY author;
+END
