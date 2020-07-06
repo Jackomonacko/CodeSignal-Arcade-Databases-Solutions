8fae48c707a22c33db8b1d0bccbcdc53b311243c
Jackomonacko <jacksadowski33@gmail.com>
2020-07-06 17:51:57 -0400

    Delete code.sql

--- WHEN was it the CASE/orderOfSuccession/code.sql
+++
@@ -1,7 +0,0 @@
-/*Please add ; after each select statement*/
-CREATE PROCEDURE orderOfSuccession()
-BEGIN
-	SELECT IF(gender = 'F', CONCAT('Queen ', name), CONCAT('King ', name)) AS name
-    FROM Successors
-    ORDER BY birthday;
-END
